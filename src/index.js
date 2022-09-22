3ab2fc885e2c33af2aa60312d82180ec84846972
feh-franc0 <fernandofv1110@gmail.com>
2022-09-22 18:52:31 -0300

    componentes

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+
+function Subtitle() {
+    return <h2>Componente Subtítulo</h2>
+}
+
+function App() {
+    return <Subtitle />
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+);
