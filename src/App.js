df0e470099fc78edfd8d2f29a6c6075ecb1d625d
feh-franc0 <fernandofv1110@gmail.com>
2022-09-22 23:51:23 -0300

    Implementando o feed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './styles/App.css';
 import './styles/PostForm.css';
+import './styles/Feed.css';
 
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
+import clockIcon from './images/clock.svg';
 
 // fontSize
 export default function App() {
+    const posts = [
+        {
+            id: Math.random(),
+            content: 'Conteúdo do post',
+            userName: 'Mateus',
+            publishedAt: new Date(),
+        },
+        {
+            id: Math.random(),
+            content: 'Este é o post2',
+            userName: 'Usuário 2',
+            publishedAt: new Date(),
+        },
+        {
+            id: Math.random(),
+            content: 'Este é o post3',
+            userName: 'Usuário 3',
+            publishedAt: new Date(),
+        },
+        {
+            id: Math.random(),
+            content: 'Este é o post4',
+            userName: 'Usuário 4',
+            publishedAt: new Date(),
+        },
+    ];
+
     return (
     <div className='wrapper'>
         <form className="post-form" onClick={() => alert('Formulário submetido')}>
@@ -23,6 +52,33 @@ export default function App() {
                 </button>
             </div>
         </form>
+
+        <main>
+            <header>
+                <h1>Seu Feed</h1>
+                <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
+            </header>
+
+            <section className="feed">
+                {posts.map((post) => (
+                    <article key={post.id}>
+                        <p> {post.content} </p>
+
+                        <footer>
+                            <div className="user-details">
+                                <img src={userIcon} alt="User" />
+                                <strong>{post.userName}</strong>
+                            </div>
+
+                            <div className="time">
+                                <img src={clockIcon} alt="Clock" />
+                                <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
+                            </div>
+                        </footer>
+                    </article>
+                ))}
+            </section>
+        </main>
     </div>
     )
 }
